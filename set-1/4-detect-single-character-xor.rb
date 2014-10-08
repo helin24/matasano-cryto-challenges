7327eba2bc2cddcc85b6ad9c51d3e391a83b689c
helin24 <helin.shiah@gmail.com>
2014-10-07 20:55:29 -0500

    detect single character

---
+++ set-1/4-detect-single-character-xor.rb
@@ -0,0 +1,28 @@
+require_relative '3-single-byte-xor-cipher'
+
+def detect(filename)
+	file = open_file(filename)
+	possibilities = []
+	file.each do |line|
+		puts line[0..-2].inspect
+		possibilities << decode(line[0..-2])
+	end
+	scored = {}
+	possibilities.each do |line|
+		scored.merge!({line => english_score(line)})
+	end
+	sorted = scored.sort_by{ |_k, value| value }
+	answer = sorted[0][0]
+end
+
+def open_file(filename)
+	file = []
+	File.open('4.txt', 'r') do |f|
+		f.each_line do |line|
+			file << line
+		end
+	end
+	file
+end
+
+
