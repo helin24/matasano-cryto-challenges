ec5ec62d3cb2e1584568339b722338385d2c345d
helin24 <helin.shiah@gmail.com>
2014-10-10 22:41:16 -0500

    decimal xor still giving wrong answer

--- set-1/5-implement-repeating-key-xor.rb
+++ set-1/5-implement-repeating-key-xor.rb
@@ -1,19 +1,37 @@
 require_relative '4-detect-single-character-xor'
 
-str1 = "Burning 'em, if you ain't quick and nimble\n"
+str1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 str2 = "I go crazy when I hear a cymbal"
 key = "ICE"
 
 def repeat_key_xor(string, key)
-	str_bin = str_to_bin_arr(string)
-	key_bin = str_to_bin_arr(key)
-	puts "string is #{str_bin} and key is #{key_bin}"
-	xor_bin = [] 
-	str_bin.each.with_index do |bin, index|
-		xor_bin << combine_bin(bin, key_bin[index % key.length])
+	str_dec = []
+	string.each_char do |c|
+		str_dec << c.ord
 	end
-	puts "xor is #{xor_bin}"
-	convert_to_hex(xor_bin.join(''))
+	key_dec = []
+	key.each_char do |c|
+		key_dec << c.ord
+	end
+	xor_dec = []
+	str_dec.each.with_index do |c, i|
+		xor_dec << ( c ^ key_dec[i % key.length] )
+	end
+	puts xor_dec.inspect
+	xor_hex = xor_dec.map do |num|
+		decimal_to_hex(num)
+	end
+	xor_hex.join('')
+	xor_dec.join('').to_i.to_s(16)
+#	str_bin = str_to_bin_arr(string)
+#	key_bin = str_to_bin_arr(key)
+#	puts "string is #{str_bin} and key is #{key_bin}"
+#	xor_bin = [] 
+#	str_bin.each.with_index do |bin, index|
+#		xor_bin << combine_bin(bin, key_bin[index % key.length])
+#	end
+#	puts "xor is #{xor_bin}"
+#	convert_to_hex(xor_bin.join(''))
 end
 
 def str_to_bin_arr(string)
@@ -24,5 +42,5 @@ def str_to_bin_arr(string)
 	bin_arr
 end
 
-puts repeat_key_xor(str1, key).length
+puts repeat_key_xor(str1, key).inspect
 puts repeat_key_xor(str2, key).inspect
